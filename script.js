e5c2efcbb902a6f074559bcb8d20a38c72108c6a
Jesse Reffsin <jreffsin@gmail.com>
2022-01-05 21:50:12 -0500

    fill out basic operator logic

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 let display = document.querySelector('#display');
 
-let first_num = 0;
-let second_num = 0;
-let active_operator = 'null';
+let first_num = null;
+let second_num = null;
+let active_operator = null;
+let clear_disp_on_next = false;
+let last_op_was_equals = false;
 
 //add event listeners to all buttons
 
@@ -16,7 +18,13 @@ const createEventListeners = () => {
 
     //digit buttons event listeners
     let digit_buttons = document.querySelectorAll('.digit_btn');
-    digit_buttons.forEach(button => button.addEventListener('click', () => display.innerText = display.innerText + button.innerText))
+    digit_buttons.forEach(button => button.addEventListener('click', () => {
+        if (clear_disp_on_next){
+            display.innerText = '';
+            clear_disp_on_next = false;
+        }
+        display.innerText = display.innerText + button.innerText;
+    }))
 
     //clear button event listener
     let clear_button = document.querySelector('#clear_button');
@@ -25,63 +33,93 @@ const createEventListeners = () => {
     //add button event listener
     let add_button = document.querySelector('#add_button');
     add_button.addEventListener('click', () => {
+        if (first_num) {
+            second_num = display.innerText;
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+        }
+
         first_num = display.innerText;
         active_operator = 'add';
+        clear_disp_on_next = true;
+        last_op_was_equals = false;
+
+        //if someone is in the middle of evaluating an expression, then
     })
 
     //subtract button event listener
     let subtract_button = document.querySelector('#subtract_button');
     subtract_button.addEventListener('click', () => {
+        if (first_num) {
+            second_num = display.innerText;
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+        }
+
         first_num = display.innerText;
         active_operator = 'subtract';
+        clear_disp_on_next = true;
+        last_op_was_equals = false;
     })
 
     //multiply button event listener
     let multiply_button = document.querySelector('#multiply_button');
     multiply_button.addEventListener('click', () => {
+        if (first_num) {
+            second_num = display.innerText;
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+        }
+
         first_num = display.innerText;
         active_operator = 'multiply';
+        clear_disp_on_next = true;
+        last_op_was_equals = false;
     })
 
     //divide button event listener
     let divide_button = document.querySelector('#divide_button');
     divide_button.addEventListener('click', () => {
+        if (first_num) {
+            second_num = display.innerText;
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+        }
+
         first_num = display.innerText;
         active_operator = 'divide';
+        clear_disp_on_next = true;
+        last_op_was_equals = false;
     })
 
     //equals button event listener
     let equals_button = document.querySelector('#equals_button');
     equals_button.addEventListener('click', () => {
-        second_num = display.innerText;
-        display.innerText = arithmetic[active_operator](first_num, second_num);
+        if (last_op_was_equals){
+            first_num = display.innerText
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+            first_num = null;
+            last_op_was_equals = true;
+        } else {
+            second_num = display.innerText;
+            display.innerText = arithmetic[active_operator](first_num, second_num);
+            clear_disp_on_next = true;
+            first_num = null;
+            last_op_was_equals = true;
+        }
     })
 
 }
 
-//define arithmetic functions
-let arithmetic ={
+//define arithmetic functions in object
+let arithmetic = {
     add: (a,b) => Number(a) + Number(b),
-    subtract: (a,b) => Number(a)-Number(b),
-    multiply: (a,b) => Number(a)*Number(b),
-    divide: (a,b) => Number(a)/Number(b)
+    subtract: (a,b) => Number(a) - Number(b),
+    multiply: (a,b) => Number(a) * Number(b),
+    divide: (a,b) => Number(a) / Number(b)
 }
 
-// let add = (a, b) => {
-//     return a + b;
-// }
-
-// let subtract = (a, b) => {
-//     return a - b;
-// }
-
-// let multiply = (a, b) => {
-//     return a * b;
-// }
-
-// let divide = (a, b) => {
-//     return a / b;
-// }
 
 createEventListeners();
 //digit buttons should show in display when pressed
